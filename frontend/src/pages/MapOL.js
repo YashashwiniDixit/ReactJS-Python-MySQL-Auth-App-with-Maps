d665d8cd2659f5a8e3347212af8f34f45c31330a
Yashashwini Dixit <yashashwinid@valuefy.com>
2022-10-18 23:55:26 +0530

    loading geojson data

--- frontend/src/pages/MapOL.js
+++ frontend/src/pages/MapOL.js
@@ -2,7 +2,11 @@ import React, { useState, useEffect, useRef } from "react";
 import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import OSM from "ol/source/OSM";
+import VectorImage from "ol/layer/VectorImage";
+import { Vector as VectorSource } from "ol/source";
+import GeoJSON from "ol/format/GeoJSON";
 import "ol/ol.css";
+import { locationData } from "./data/location.geojson";
 
 function MapOL() {
   const [map, setMap] = useState();
@@ -20,11 +24,20 @@ function MapOL() {
       ],
       view: new View({
         center: [8112034.988467557, 2163800.7904075133],
-        zoom: 7,
-        maxZoom: 10,
-        minZoom: 5,
+        zoom: 2,
       }),
     });
+    // Vector Layers
+    const locationLayer = new VectorImage({
+      source: new VectorSource({
+        url: locationData,
+        format: new GeoJSON(),
+      }),
+      visible: true,
+      title: "Location Layer",
+    });
+    initialMap.addLayer(locationLayer);
+    console.log("initialMap", initialMap);
     setMap(initialMap);
     initialMap.on("click", (e) => {
       console.log(e.coordinate);
